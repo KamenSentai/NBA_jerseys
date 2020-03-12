1053fec862fc53a8e979e524f35217e41d4873b7
KamenSentai <cvt.alain@gmail.com>
2020-03-12 17:39:50 +0100

    feat(utils): add all teams

--- src/utils/teams.js
+++ src/utils/teams.js
@@ -1,10 +1,66 @@
 export default [
   {
+    name: 'Atlanta Hawks',
+    slug: 'hawks',
+    colors: [
+      '#E03A3E',
+      '#C1D32F',
+    ],
+  },
+  {
+    name: 'Boston Celtics',
+    slug: 'celtics',
+    colors: [
+      '#007A33',
+      '#BA9653',
+    ],
+  },
+  {
+    name: 'Brooklyn Nets',
+    slug: 'nets',
+    colors: [
+      '#000000',
+      '#FFFFFF',
+    ],
+  },
+  {
+    name: 'Charlotte Hornets',
+    slug: 'hornets',
+    colors: [
+      '#1D1160',
+      '#00788C',
+    ],
+  },
+  {
     name: 'Chicago Bulls',
     slug: 'bulls',
     colors: [
       '#CE1141',
       '#000000',
+    ],
+  },
+  {
+    name: 'Cleveland Cavaliers',
+    slug: 'cavaliers',
+    colors: [
+      '#860038',
+      '#041E42',
+    ],
+  },
+  {
+    name: 'Dallas Mavericks',
+    slug: 'mavericks',
+    colors: [
+      '#00538C',
+      '#002B5E',
+    ],
+  },
+  {
+    name: 'Denver Nuggets',
+    slug: 'nuggets',
+    colors: [
+      '#0E2240',
+      '#FEC524',
     ],
   },
   {
@@ -16,11 +72,171 @@ export default [
     ],
   },
   {
+    name: 'Golden State Warriors',
+    slug: 'warriors',
+    colors: [
+      '#1D428A',
+      '#FFC72C',
+    ],
+  },
+  {
+    name: 'Houston Rockets',
+    slug: 'rockets',
+    colors: [
+      '#CE1141',
+      '#000000',
+    ],
+  },
+  {
+    name: 'Indiana Pacers',
+    slug: 'pacers',
+    colors: [
+      '#002D62',
+      '#FDBB30',
+    ],
+  },
+  {
+    name: 'Los Angeles Clippers',
+    slug: 'clippers',
+    colors: [
+      '#C8102E',
+      '#1D428A',
+    ],
+  },
+  {
     name: 'Los Angeles Lakers',
     slug: 'lakers',
     colors: [
       '#FDB927',
       '#552583',
+    ],
+  },
+  {
+    name: 'Memphis Grizzlies',
+    slug: 'grizzlies',
+    colors: [
+      '#5D76A9',
+      '#12173F',
+    ],
+  },
+  {
+    name: 'Miami Heat',
+    slug: 'heat',
+    colors: [
+      '#98002E',
+      '#F9A01B',
+    ],
+  },
+  {
+    name: 'Milwaukee Bucks',
+    slug: 'bucks',
+    colors: [
+      '#00471B',
+      '#EEE1C6',
+    ],
+  },
+  {
+    name: 'Minnesota Timberwolves',
+    slug: 'timberwolves',
+    colors: [
+      '#0C2340',
+      '#236192',
+    ],
+  },
+  {
+    name: 'New Orleans Pelicans',
+    slug: 'pelicans',
+    colors: [
+      '#0C2340',
+      '#C8102E',
+    ],
+  },
+  {
+    name: 'New York Knicks',
+    slug: 'knicks',
+    colors: [
+      '#006BB6',
+      '#F58426',
+    ],
+  },
+  {
+    name: 'Oklahoma City Thunder',
+    slug: 'thunders',
+    colors: [
+      '#007AC1',
+      '#EF3B24',
+    ],
+  },
+  {
+    name: 'Orlando Magic',
+    slug: 'magicians',
+    colors: [
+      '#0077C0',
+      '#C4CED4',
+    ],
+  },
+  {
+    name: 'Philadelphia 76ers',
+    slug: '76ers',
+    colors: [
+      '#006BB6',
+      '#ED174C',
+    ],
+  },
+  {
+    name: 'Phoenix Suns',
+    slug: 'suns',
+    colors: [
+      '#1D1160',
+      '#E56020',
+    ],
+  },
+  {
+    name: 'Portland Trail Blazers ',
+    slug: 'trail blazers',
+    colors: [
+      '#E03A3E',
+      '#000000',
+    ],
+  },
+  {
+    name: 'Sacramento Kings',
+    slug: 'kings',
+    colors: [
+      '#5A2D81',
+      '#63727A',
+    ],
+  },
+  {
+    name: 'San Antonio Spurs',
+    slug: 'spurs',
+    colors: [
+      '#C4CED4',
+      '#000000',
+    ],
+  },
+  {
+    name: 'Toronto Raptors',
+    slug: 'raptors',
+    colors: [
+      '#CE1141',
+      '#000000',
+    ],
+  },
+  {
+    name: 'Utah Jazz',
+    slug: 'jazz',
+    colors: [
+      '#002B5C',
+      '#00471B',
+    ],
+  },
+  {
+    name: 'Washington Wizards',
+    slug: 'wizards',
+    colors: [
+      '#002B5C',
+      '#E31837',
     ],
   },
 ]

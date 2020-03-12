dfdc271c30cd74687e05dc9a925a5dd35145e42b
KamenSentai <cvt.alain@gmail.com>
2020-03-12 12:41:41 +0100

    feat(utils): add teams data

---
+++ src/utils/teams.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    name: 'Chicago Bulls',
+    slug: 'bulls',
+    colors: [
+      '#CE1141',
+      '#000000',
+    ],
+  },
+  {
+    name: 'Detroit Pistons',
+    slug: 'detroit',
+    colors: [
+      '#1D42BA',
+      '#C8102E',
+    ],
+  },
+  {
+    name: 'Los Angeles Lakers',
+    slug: 'lakers',
+    colors: [
+      '#FDB927',
+      '#552583',
+    ],
+  },
+]
